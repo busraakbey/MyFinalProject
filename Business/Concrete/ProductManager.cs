41aea6a845963d6dbdc6b098a9164ae3301fb184
Büşra Akbey <busraakbey3@gmail.com>
2021-03-20 16:33:35 +0300

    6 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -34,6 +35,10 @@ namespace Business.Concrete
         {
             return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
         }
-        
+
+        public List<ProductDetailDto> GetProductDetails()
+        {
+            return _productDal.GetProductDetails();
+        }
     }
 }
