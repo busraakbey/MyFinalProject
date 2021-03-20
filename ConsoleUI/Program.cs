e58076e0ee466b381fdfe4011c6d86a1fbda2b77
Büşra Akbey <busraakbey3@gmail.com>
2021-03-20 15:17:35 +0300

    6 Şubat Ders Sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,13 +11,22 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetByUnitPrice(40,1100))
+            foreach (var product in productManager.GetByUnitPrice(40, 1100))
             {
                 Console.WriteLine(product.ProductName);
             }
-
-            
         }
     }
 }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,15 +1,15 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         //generic constraint
-        //class v: referans tip
+        //class : referans tip
         //IEntity : IEntity olabilir veya IEntity implemente eden bir nesne olabilir
         //new() : new'lenenilir olmalı
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
